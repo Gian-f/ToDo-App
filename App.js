840f358454230f432e3a2bcf3e2a8a876d4b39b2
Gian-f <gianfelipe87@gmail.com>
2022-06-02 15:22:47 -0300

    Creating the main class

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from './Colors';
+import tempData from './tempData';
+import TodoList from './components/TodoList';
+import AddListModal from './components/AddListModal';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    addTodoVisible: false
+  }
+
+  toggleAddTodoModal() {
+    this.setState({ addTodoVisible: !this.state.addTodoVisible });
+  }
+
+  renderList = list => {
+    return <TodoList list={list} />
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Modal animationType="slide" visible={this.state.addTodoVisible}
+        onRequestClose={() => this.toggleAddTodoModal()}>
+
+          <View style={styles.modal}>
+              <AddListModal closeModal= {() => this.toggleAddTodoModal()} />
+          </View>
+
+        </Modal>
+
+        <View style={{ flexDirection: "row" }}>
+
+          <View style={styles.divider} />
+
+          <Text style={styles.title}>
+            Todo <Text style={{ fontWeight: "300", color: colors.blue }}> Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}> Add List </Text>
+        </View>
+
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={item => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item)}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +72,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+
+  title: {
+    fontSize: 38,
+    fontWeight: "300",
+    color: colors.black,
+    paddingHorizontal: 64
+  },
+
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
+  }
 });
